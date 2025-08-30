c1423f139060a3fe9d2ecacd0686290e24ca53c9
Almagribii <brucadalm@gmail.com>
2025-08-30 10:30:56 +0700

    create footer

---
+++ react/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#282c34",
+        color: "white",
+        padding: "1rem",
+        textAlign: "center",
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+      }}
+    >
+        <p>&copy; 2025 Website Saya</p>
+    </footer>
+  );
+}
+
+export default Footer;
