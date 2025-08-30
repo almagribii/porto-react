782c0c38499eac92d2f254d4e1eb97eb4b585fe4
Almagribii <brucadalm@gmail.com>
2025-08-30 10:13:12 +0700

    create component

---
+++ react/src/components/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+    return <header style={{background: '#282c34', color: 'white', padding: '1rem'}}>
+        <h1>Selamat Datang di aplikasi saya</h1>
+    </header>
+}
+
+export default Header;
