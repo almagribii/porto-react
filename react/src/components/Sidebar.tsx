6a6eda2e12b009fa4a0e9cee5dca498938e2146e
Almagribii <brucadalm@gmail.com>
2025-08-30 10:26:08 +0700

    create Sidebar

---
+++ react/src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+function Sidebar() {
+    return (
+        <aside style={{width: '250px', padding: '1rem', backgroundColor: '#f0f0f0'}}>
+            <h2>Navigasi</h2>
+            <ul>
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Tentang Kami</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+        </aside>
+    )
+}
